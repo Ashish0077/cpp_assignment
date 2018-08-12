a2a871d5ecaade65e5ab7af8ca02bf07467a6520
Ashish Arora <ashish007722@gmail.com>
2018-08-12 07:17:53 -0700

    Assignment Ques 9.2
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

---
+++ Ques9_2.cpp
@@ -0,0 +1,55 @@
+/*
+Write aprogram to add two strings using the + operator
+*/
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class String
+{
+public:
+	char str[20];
+public:
+	void accept_string()
+	{
+		cout << "\n Enter String :\n";
+		cin >> str;
+	}
+	void display_string()
+	{
+		cout << str;
+	}
+	String operator+(String x)  //Concatenating String
+	{
+		String s;
+		strcat(str, x.str);
+		strcpy(s.str, str);
+		return s;
+	}
+};
+
+int main()
+{
+	String str1, str2, str3;
+
+	str1.accept_string();
+	str2.accept_string();
+
+	cout << "\n ----------------------------------------------";
+	cout << "\n\n First String is           :  ";
+	str1.display_string();   //Displaying First String
+
+	cout << "\n\n Second String is          :  ";
+	str2.display_string();  //Displaying Second String
+
+	cout << "\n ----------------------------------------------";
+	str3 = str1 + str2;         //String is concatenated. Overloaded '+' operator
+	cout << "\n\n Concatenated String is    :  ";
+	str3.display_string();
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
